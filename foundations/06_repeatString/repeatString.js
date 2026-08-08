1febf8e856cfde72323c3c5eb035a6ceea927be6
Avşar Zırh <avsarzrh.1990@gmail.com>
2026-08-08 17:47:30 +0300

    06-repeatString is completed

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if (times < 0) return 'ERROR';
+    let word = '';
+    for (i = 0; i < times; i++) {
+        word += str;
+    }
+    return word;
 };
+console.log(repeatString('hey', 3))
+
 
 // Do not edit below this line
 module.exports = repeatString;
